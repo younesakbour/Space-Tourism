ecc4e3cfa582e4abd84693f595ae0fe5d2cc4d29
younes <younesakbour6@gmail.com>
2024-02-05 19:35:01 +0100

    Added 2 arrays with information for each crew and technology

--- app/components/Crew/Section.jsx
+++ app/components/Crew/Section.jsx
@@ -2,6 +2,32 @@
 import { motion } from "framer-motion"
 
 export default function Section() {
+  const crews = [
+    {
+      position: "Commander",
+      name: "Douglas Hurley",
+      descr: "Douglas Gerald Hurley is an American engineer, former Marine Corps pilot and former NASA astronaut. He launched into space for the third time as commander of Crew Dragon Demo-2.",
+      image: "https://res.cloudinary.com/daxdgprva/image/upload/v1705266025/front-end/space-tourism-website/crew/mve6ybbvdcywggfb91q4.webp",
+    },
+    {
+      position: "Mission Specialist",
+      name: "MARK SHUTTLEWORTH",
+      descr: "Mark Richard Shuttleworth is the founder and CEO of Canonical, the company behind the Linux-based Ubuntu operating system. Shuttleworth became the first South African to travel to space as a space tourist.",
+      image: "https://res.cloudinary.com/daxdgprva/image/upload/v1705266038/front-end/space-tourism-website/crew/ktxc0v6u4gf4f0mc73uy.webp",
+    },
+    {
+      position: "PILOT",
+      name: "Victor Glover",
+      descr: "Pilot on the first operational flight of the SpaceX Crew Dragon to the International Space Station. Glover is a commander in the U.S. Navy where he pilots an F/A-18.He was a crew member of Expedition 64, and served as a station systems flight engineer.",
+      image: "https://res.cloudinary.com/daxdgprva/image/upload/v1705266052/front-end/space-tourism-website/crew/ixe48cijoxywfvyt6xi3.webp",
+    },
+    {
+      position: "Flight Engineer",
+      name: "Anousheh Ansari",
+      descr: "Anousheh Ansari is an Iranian American engineer and co-founder of Prodea Systems. Ansari was the fourth self-funded space tourist, the first self-funded woman to fly to the ISS, and the first Iranian in space.",
+      image: "https://res.cloudinary.com/daxdgprva/image/upload/v1705266055/front-end/space-tourism-website/crew/av4dcfdohxpt3x5sdavm.webp",
+    },
+  ]
   return (
     <div className='h-screen w-full lg:max-w-[1280px] pt-24 md:pt-32 lg:pt-[10em] mx-auto p-8 md:p-0 max-h-screen flex flex-col items-center md:items-start'>
       <div className='flex gap-4 md:text-xl lg:text-[28px] uppercase tracking-[2.7px] md:tracking-[3.38px] lg:tracking-[4.72px] p-8'><span className='opacity-25'>02</span><h2>Meet your crew</h2></div>
--- app/components/Technology/Section.jsx
+++ app/components/Technology/Section.jsx
@@ -2,6 +2,26 @@
 import { motion } from "framer-motion"
 
 export default function Section() {
+  const technologies = [
+    {
+      name: "LAUNCH VEHICLE",
+      descr: "A launch vehicle or carrier rocket is a rocket-propelled vehicle used to carry a payload from Earth's surface to space, usually to Earth orbit or beyond. Our WEB-X carrier rocket is the most powerful in operation. Standing 150 metres tall, it's quite an awe-inspiring sight on the launch pad!",
+      imagePortrait: "https://res.cloudinary.com/daxdgprva/image/upload/v1705266185/front-end/space-tourism-website/technology/mwdcttweiikrhfz4fch7.jpg",
+      imageLandscape: "https://res.cloudinary.com/daxdgprva/image/upload/v1705266149/front-end/space-tourism-website/technology/kw2qyja0f8u3fmpjlnta.jpg",
+    },
+    {
+      name: "SPACEPORT",
+      descr: "A spaceport or cosmodrome is a site for launching (or receiving) spacecraft, by analogy to the seaport for ships or airport for aircraft. Based in the famous Cape Canaveral, our spaceport is ideally situated to take advantage of the Earth’s rotation for launch.",
+      imagePortrait: "https://res.cloudinary.com/daxdgprva/image/upload/v1705266247/front-end/space-tourism-website/technology/ixs159v7nn7xjmdak8sl.jpg",
+      imageLandscape: "https://res.cloudinary.com/daxdgprva/image/upload/v1705266176/front-end/space-tourism-website/technology/jnjmdzme515vsup9ypv2.jpg",
+    },
+    {
+      name: "SPACE CAPSULE",
+      descr: "A space capsule is an often-crewed spacecraft that uses a blunt-body reentry capsule to reenter the Earth's atmosphere without wings. Our capsule is where you'll spend your time during the flight. It includes a space gym, cinema, and plenty of other activities to keep you entertained.",
+      imagePortrait: "https://res.cloudinary.com/daxdgprva/image/upload/v1705266156/front-end/space-tourism-website/technology/ukyyrfibmcqshes2nwgf.jpg",
+      imageLandscape: "https://res.cloudinary.com/daxdgprva/image/upload/v1705266184/front-end/space-tourism-website/technology/mfrnvrvn5amgpmrfuqic.jpg",
+    },
+  ]
   return (
     <div className='h-screen w-full lg:max-w-[1280px] pt-24 md:pt-32 lg:pt-[10em] mx-auto lg:p-8 max-h-screen flex flex-col items-center md:items-start'>
       <div className='flex gap-4 md:text-xl lg:text-[28px] uppercase tracking-[2.7px] md:tracking-[3.38px] lg:tracking-[4.72px]'><span className='opacity-25'>03</span><h2>SPACE LAUNCH 101</h2></div>
